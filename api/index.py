f434f4710eb1257d283212553f8df858cb0ba988
FallenGME <mrhello2227222@gmail.com>
2025-08-27 01:29:15 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 import os
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -11,11 +12,3 @@ def index():
         "status": "ok",
         "Github": "https://github.com/FallenGME/RbxCord/issues"
     })
-
-@app.errorhandler(404)
-def not_found(e):
-    return jsonify({
-        "message": "RbxCord is running. This is a private website.",
-        "status": "ok",
-        "Github": "https://github.com/FallenGME/RbxCord/issues"
-    }), 404
