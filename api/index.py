cc7176a6e01df0276b3f69b44b2542e4a1e6d550
FallenGME <mrhello2227222@gmail.com>
2025-08-27 01:27:52 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,6 +4,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 os.environ.get("REPLICATE_API_TOKEN")
 
+@app.route("/")
+def index():
+    return jsonify({
+        "message": "RbxCord is running. This is a private website.",
+        "status": "ok",
+        "Github": "https://github.com/FallenGME/RbxCord/issues"
+    })
+
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({
